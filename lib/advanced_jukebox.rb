43899cf7c69741849ad8d5efa3cc22746cf6ef93
Diana Utan <dia_utzi@yahoo.com>
2019-03-12 01:38:09 +0000

    Automatically backed up by Learn

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -27,6 +27,8 @@ end
 def list(my_songs)
   #this method is different! Collect the keys of the my_songs hash and 
   #list the songs by name
+  my_songs.each do |song, location|
+    puts "#{song}"
 end
 
 
